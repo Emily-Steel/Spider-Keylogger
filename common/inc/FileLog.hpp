fc5b55b8c4b65cc89254769da5af5af37b5b4987
Etienne DEBAS <etienne.debas@epitech.eu>
2014-10-28 20:44:58 +0100

    remove virtual pur method from FileLog

--- common/inc/FileLog.hpp
+++ common/inc/FileLog.hpp
@@ -14,8 +14,8 @@ public:
     virtual void open(const std::string &path);
     virtual void close();
     virtual bool isGood() const;
-    virtual void insert(const APacket &, const std::string &id) = 0;
-    virtual std::vector<APacket *> dump() = 0;
+    virtual void insert(const APacket &, const std::string &id);
+    virtual std::vector<APacket *> dump();
 
 private:
     std::fstream    _fileHandle;
