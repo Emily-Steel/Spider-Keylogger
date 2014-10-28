95a94a2264bf9356fc7f1b1c8ce194a753b4297a
Etienne DEBAS <etienne.debas@epitech.eu>
2014-10-28 21:08:43 +0100

    add Network and server class

---
+++ server/src/Server.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <FileLog.hpp>
+#include "Server.hpp"
+
+Server::Server(const std::string &logPath) {
+    std::string fileName = "";
+    size_t found;
+
+    found = logPath.find_last_of("/\\");
+    if (found != std::string::npos) {
+        fileName = logPath.substr(found + 1);
+    }
+    else {
+        fileName = logPath;
+    }
+    found = fileName.find_last_of(".");
+    if (found != std::string::npos) {
+        std::string type = fileName.substr(found + 1);
+        if (type.compare("json") == 0) {
+            std::cout << "JSON FILE" << std::endl;
+            std::unique_ptr<FileLog> filelog(new FileLog());
+            log = std::move(filelog);
+            log->open(logPath);
+        }
+        else if (type.compare("db") == 0) {
+            std::cout << "DATABASE FILE" << std::endl;
+        }
+        else {
+            throw std::invalid_argument("Invalid log file type [" + fileName + "]" + "\n" + "File type must be .db or .json");
+        }
+    }
+    else {
+        throw std::invalid_argument("Invalid log file type [" + fileName + "]" + "\n" + "File type must be .db or .json");
+    }
+}
+
+Server::~Server() {
+}
+
+bool Server::run() {
+    return false;
+}
+
+void Server::handle_input() {
+
+}
+
+void Server::pollCallback(ALog *log, const std::string &clientId, APacket &packet) {
+
+}
