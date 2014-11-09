186611b5c6a92ffe912df84f3f54b64f417157f2
Christopher Steel <steel_c@epitech.eu>
2014-11-09 20:50:41 +0100

    Removed diffie-hellman for compatibility reasons

--- common/src/BoostSslCtxServer.cpp
+++ common/src/BoostSslCtxServer.cpp
@@ -9,7 +9,6 @@ BoostSslCtxServer::BoostSslCtxServer()
   try {
     _ctx.use_certificate_chain_file("certs/server.crt");
     _ctx.use_private_key_file("certs/server.key", boost::asio::ssl::context::pem);
-    _ctx.use_tmp_dh_file("certs/dh512.pem");
   } catch (const boost::system::system_error& e) {
     std::cerr << "failed to load server-side certificate: " << e.what() << std::endl;
   }
